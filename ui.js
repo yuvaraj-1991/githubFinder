cfa3cc69e71d497297d35a1982f70d7f00edbb4c
Yuvaraj <64359176+yuvaraj-1991@users.noreply.github.com>
2020-07-11 20:09:49 +0530

    few changes made

--- ui.js
+++ ui.js
@@ -32,6 +32,31 @@ class UI {
         `
     }
 
+    //Show User Repos
+    showRepos(repos) {
+        let output = ''
+
+        repos.forEach(function(repo) {
+            output += `
+                <div class="card card-body mb-2"> 
+                    <div class="row">
+                        <div class="col-md-6"> 
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6"> 
+                        <span class="badge badge-primary">Starts: ${repo.stargazers_count}</span>
+                        <span class="badge badge-secondary">Watchers: ${repo.watchers}</span>
+                        <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `
+        })
+
+        //Output repos
+        document.getElementById('repos').innerHTML = output
+    }
+
 
     //Show Alert
     showAlert(message, className) {
